103b0f43460d2ab2100b17a987dee152e435c6a2
Michael Vitollo <ace51689@gmail.com>
2021-03-16 10:03:26 -0400

    Created new branch update-user

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -1,3 +1,4 @@
+import { useStore } from './store/store.js'
 const baseURL = "https://socialapp-api.herokuapp.com/";
 
 export const loginRequest = (username, password) => {
@@ -17,3 +18,16 @@ export const logoutRequest = (token) => {
     headers: { Authorization: "Bearer " + token },
   }).then((res) => res.json());
 };
+
+export const updateUser = (currName, newName, displayName, password) => {
+  return fetch(baseURL + currName, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      newName,
+      displayName,
+      password,
+    }),
+  })
+    .then((res) => res.json())
+}
