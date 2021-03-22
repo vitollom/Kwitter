f0001a85b296da29757f944eba8ad7f3c3bbc519
Michael Vitollo <ace51689@gmail.com>
2021-03-22 15:01:51 -0400

    Created an UploadPicture component and associated fetch request. Allows a user to upload/update their profile picture.

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -2,7 +2,7 @@ const baseURL = "https://socialapp-api.herokuapp.com/";
 const backupURL = "https://kwitter-api-b.herokuapp.com/";
 
 export const loginRequest = (username, password) => {
-  return fetch(backupURL + "auth/login", {
+  return fetch(baseURL + "auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -13,20 +13,20 @@ export const loginRequest = (username, password) => {
 };
 
 export const logoutRequest = (token) => {
-  return fetch(backupURL + "auth/logout", {
+  return fetch(baseURL + "auth/logout", {
     headers: { Authorization: "Bearer " + token },
   }).then((res) => res.json());
 };
 
 export const getUser = (username) => {
-  return fetch(backupURL + "users/" + username, {
+  return fetch(baseURL + "users/" + username, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   }).then((res) => res.json());
 };
 
 export const updateRequest = (token, username, updateData) => {
-  return fetch(backupURL + "users/" + username, {
+  return fetch(baseURL + "users/" + username, {
     method: "PATCH",
     headers: {
       Authorization: `Bearer ${token}`,
@@ -66,18 +66,28 @@ export const deleteUser = (username, token) => {
   return fetch(backupURL + "users/" + username, {
     method: "DELETE",
     headers: { Authorization: "Bearer " + token },
-}).then((res) => res.json());
+  }).then((res) => res.json());
 };
 
 export const createMessage = (message, token) => {
-  console.log(message)
   return fetch(backupURL + "messages", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + token,
-    }, 
+    },
     body: JSON.stringify({ text: message }),
   }).then((res) => res.json());
 };
-  
+
+export const uploadPicture = (username, token, pictureData) => {
+  let formData = new FormData()
+  formData.append("picture", pictureData)
+  return fetch(baseURL + "users/" + username + "/picture", {
+    method: "PUT",
+    headers: {
+      Authorization: "Bearer " + token
+    },
+    body: formData,
+  }).then((res) => res.json())
+}
