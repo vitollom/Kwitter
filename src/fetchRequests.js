8e58c8ba3d4764104e7b9fb172f67f80e357be3d
Kirill Savvun <ksavv10@gmail.com>
2021-03-21 21:24:46 -0400

    test

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -21,28 +21,27 @@ export const logoutRequest = (token) => {
 export const getUser = (username) => {
   return fetch(backupURL + "users/" + username, {
     method: "GET",
-    headers: {"Content-Type": "application/json"}
+    headers: { "Content-Type": "application/json" },
   }).then((res) => res.json());
-}
+};
 
 export const updateRequest = (token, username, updateData) => {
   return fetch(backupURL + "users/" + username, {
     method: "PATCH",
-    headers: { Authorization: `Bearer ${token}`, 
-              "Content-Type": "application/json" },
-    body: JSON.stringify(
-      updateData
-    ),
-  })
-    .then((res) => res.json())
-}
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updateData),
+  }).then((res) => res.json());
+};
 
 export const messageList = () => {
   return fetch(backupURL + "messages?limit=100", {
     method: "GET",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
   }).then((res) => res.json());
-}
+};
 
 export const createUser = (username, displayName, password) => {
   return fetch(backupURL + "users", {
@@ -60,5 +59,5 @@ export const deleteMessage = (messageId, token) => {
   return fetch(backupURL + "messages/" + messageId, {
     method: "DELETE",
     headers: { Authorization: "Bearer " + token },
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
