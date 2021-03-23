714e4e60e1f7dcc2deb0db8f4c0ba42ded431a7b
Michael Vitollo <ace51689@gmail.com>
2021-03-23 10:33:06 -0400

    Added polling for updates functionallity on the MessageList page(component). Auto refreshes message list every 30 seconds, and resets that timer whenver a message is posted or deleted.

--- src/views/MessageList.js
+++ src/views/MessageList.js
@@ -1,42 +1,60 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useStore, GETMESSAGES } from '../store/store';
 import { messageList } from '../fetchRequests';
 import Menu from '../components/Menu.js'
 import MessageItem from "../components/MessageItem.js";
 import CreateMessage from "../components/CreateMessage";
 
-
-
 function MessageList(props) {
   const dispatch = useStore(state => state.dispatch)
   const messages = useStore(state => state.messageData.messages)
+
+  const [refresh, setRefresh] = useState(30)
+  const refreshRef = useRef(refresh)
+  refreshRef.current = refresh
 
   const handleMessages = () => {
     messageList().then((messageData) =>
       dispatch({ type: GETMESSAGES, payload: messageData })
     );
   };
-  
+
   useEffect(() => {
     handleMessages();
     return () => handleMessages()
-  }, [dispatch])
+  }, [])
+
+  useEffect(() => {
+    window.timer = setInterval(() => {
+      if (refreshRef.current === 0) {
+        handleMessages()
+        setRefresh(30)
+      } else {
+        setRefresh(refresh => refresh - 1)
+      }
+    }, 1000)
+
+    return () => clearInterval(window.timer)
+
+  }, [setRefresh])
 
 
   return (
     <>
-    <Menu />
-    <CreateMessage  handleMessages={handleMessages} />
-    <ul>
-      {messages && messages.map((message) => (
-        <MessageItem 
-          {...message}
-          key={message.id}
-          message={message}
-          handleMessages={handleMessages}
-        />
-      ))}
-    </ul>
+      <Menu />
+      <CreateMessage handleMessages={handleMessages} setRefresh={setRefresh} />
+      <p>Message list refreshing in {refresh} second(s)</p>
+      <ul>
+        {messages && messages.map((message) => (
+          <MessageItem
+            {...message}
+            key={message.id}
+            message={message}
+            handleMessages={handleMessages}
+            setRefresh={setRefresh}
+          />
+        ))}
+      </ul>
     </>
   )
 }
