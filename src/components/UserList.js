42fb22b0bcc4b5dcc6139fef3c891bec951c8b0d
Michael Vitollo <ace51689@gmail.com>
2021-03-25 20:52:34 -0400

    The users list on the profile page now displays photos of each user if one exists.

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { userList } from "../fetchRequests";
 import { Card } from "react-bootstrap";
+import NoPicture from "../assets/Pictures/unknown_user_image.png"
 
 function UserList() {
+  const baseURL = "https://socialapp-api.herokuapp.com";
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -15,8 +17,16 @@ function UserList() {
     <div className="kwitter-user-list">
       <h1 className="kwitter-new-users">Kwitter's Newest Users!</h1>
       {users.map((user) => {
-        return <Card>
-          <Card.Body>
+        return <Card className="kwitter-user-card">
+          <Card.Body className="kwitter-users-left">
+            {
+              user.pictureLocation ?
+                <Card.Img className="users-list-picture" src={baseURL + user.pictureLocation}></Card.Img>
+                :
+                <Card.Img className="users-list-picture" src={NoPicture}></Card.Img>
+            }
+            </Card.Body>
+          <Card.Body className="kwitter-users-right">
             <Card.Title>{user.username}</Card.Title>
             <Card.Text>{user.about}</Card.Text>
           </Card.Body>
